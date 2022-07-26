a71223f3cff6ef3f339a842afabdacc3fd2924b7
Berkay Celik <berkaycelik66@hotmail.com>
2022-07-26 23:33:13 +0300

    Diziler-MaxMin added

--- Diziler-MaxMinEleman/src/MaxMinBulma.java
+++ Diziler-MaxMinEleman/src/MaxMinBulma.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class MaxMinBulma{
+public class MaxMinBulma {
 
-    public static void main(String []args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] dizi = {15,12,788,1,-1,-778,2,0};
+        int[] dizi = {15, 12, 788, 1, -1, -778, 2, 0};
 
         int maxYakin = dizi[0];
-        int minYakin  = dizi[0];
+        int minYakin = dizi[0];
 
         System.out.print("Bir Sayi Giriniz: ");
         int girilenSayi = input.nextInt();
 
         Arrays.sort(dizi);
-        for(int i: dizi){
-            if(i <= girilenSayi){
+        for (int i : dizi) {
+            if (i <= girilenSayi) {
                 minYakin = i;
             }
 
-            if(i >= girilenSayi) {
+            if (i >= girilenSayi) {
                 maxYakin = i;
                 break;
             }
