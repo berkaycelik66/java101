32e5a45b52eb48b318557658440632d933aab2c1
Berkay Celik <berkaycelik66@hotmail.com>
2022-07-26 13:35:37 +0300

    RecursiveFibonacci added

---
+++ RecursiveFibonnacciSerisi/src/RecursiveFibonacci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class RecursiveFibonacci {
+    public static int fibonacci(int n){
+        if(n==1 || n==2) {
+            return 1;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Fibonacci Serisinin Kaçıncı Elemanını Istediğinizi Giriniz: ");
+        int sayi = input.nextInt();
+
+        System.out.println(fibonacci(sayi));
+    }
+}
